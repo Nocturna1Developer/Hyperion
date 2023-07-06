2da1b92dd63d8b69893f90254ef1cbd9f85496c9
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2023-07-06 16:26:49 -0700

    added react icons and also added react-icons

--- hyperion-music-app/app/layout.tsx
+++ hyperion-music-app/app/layout.tsx
@@ -1,21 +1,73 @@
-import './globals.css'
+/* 
+Layout.tsx:
+
+- The Layout component defines the main layout and structure of the app.
+- It fetches data, provides contexts, and renders the main components of the app like Sidebar and Player.
+- It uses Google's Figtree font, which is configured and set for the body of the application.
+- The component also defines metadata for the application and a revalidation value for incremental static regeneration.
+
+Details:
+
+- The component fetches active products with prices and songs associated with the user using `getActiveProductsWithPrices` and `getSongsByUserId` respectively.
+- These data are then used to provide context and props for children components.
+- The `ToasterProvider`, `SupabaseProvider`, and `UserProvider` wrap the whole application for providing application-wide context for toast notifications, Supabase client, and user state respectively.
+- The `ModalProvider` uses fetched products as props.
+- The `Sidebar` and `Player` are main components of the app and are rendered within the `UserProvider` context.
+- The layout is wrapped in html and body tags, with the body tag being assigned the Figtree font.
+*/
+
 import { Figtree } from 'next/font/google'
 
-const font = Figtree({ subsets: ['latin'] })
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrices'
+import Sidebar from '@/components/Sidebar'
+import ToasterProvider from '@/providers/ToasterProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import Player from '@/components/Player'
+
+import './globals.css'
+
+const font = Figtree({ subsets: ['latin'] }) // Use Figtree Google font with the 'latin' subset.
 
 export const metadata = {
   title: 'H Y P E R I O N',
   description: 'A minimalistic music streaming app.',
 }
 
-export default function RootLayout({
+export const revalidate = 0;
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const products = await getActiveProductsWithPrices(); // Fetch all active products with their prices.
+  const userSongs = await getSongsByUserId(); // Fetch all songs for the user.
+
+  // The RootLayout return structure.
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        {/* Toast notification context */}
+        <ToasterProvider />
+        {/* Supabase client context */}
+        <SupabaseProvider>
+          {/* User context */}
+          <UserProvider>
+            {/* Modal context with fetched products */}
+            <ModalProvider products={products} />
+            {/* Sidebar component with user songs */}
+            <Sidebar songs={userSongs}>
+              {/* Child components */}
+              {children}
+            </Sidebar>
+            {/* Player component */}
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   )
 }
--- hyperion-music-app/components/Sidebar.tsx
+++ hyperion-music-app/components/Sidebar.tsx
@@ -1,16 +1,85 @@
+/* 
+Sidebar.tsx:
+
+- The Sidebar component renders the sidebar of the app and handles routing to different pages (Home and Search).
+- It also displays the list of songs in the user's library using the Library component.
+- The component uses the Tailwind CSS utility for styles and the `usePathname` hook from Next.js to get the current path name for navigation.
+- It uses the `usePlayer` hook to access the activeId state of the Player.
+- A `useMemo` hook is used to optimize the creation of the `routes` array, which is dependent on the `pathname`.
+
+Details:
+
+- It takes two props: `children` for rendering child components and `songs` which is an array of Song objects.
+- The component contains two main sections: The sidebar and the main content area. The sidebar includes navigational items and the library of songs. The main content area is where child components are rendered.
+- The `routes` array defines the configuration for the sidebar navigation items, including their icons, labels, active state, and href values.
+- A `twMerge` function is used to conditionally change the height of the component based on the `player.activeId` state.
+*/
+
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { twMerge } from "tailwind-merge";
+import { usePathname } from "next/navigation";
+
+import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+
+import SidebarItem from "./SidebarItem";
+import Box from "./Box";
+import Library from "./Library";
+import { useMemo } from "react";
+
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[];
 }
 
-const Sidebar: React.FC<SidebarProps> = ({
-    children
-}) => {
-    return (
+const Sidebar = ({ children, songs }: SidebarProps) => {
+    const pathname = usePathname(); // Get the current pathname
+    const player = usePlayer(); // Get the player context
 
-        <div>
-            {children}
+    // Define the configuration for the sidebar navigation items
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/'
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            href: '/search',
+            active: pathname === '/search'
+        },
+    ], [pathname]);
+
+    return (
+        <div
+            // Apply different styles based on the active state of the player
+            className={twMerge(`flex h-full`, player.activeId && 'h-[calc(100%-80px)]')}
+        >
+            {/* The sidebar */}
+            <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+                <Box>
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {/* Sidebar navigation items */}
+                        {routes.map((item) => (
+                            <SidebarItem key={item.label} {...item} />
+                        ))}
+                    </div>
+                </Box>
+                {/* User's library of songs */}
+                <Box className="overflow-y-auto h-full">
+                    <Library songs={songs} />
+                </Box>
+            </div>
+
+            {/* The main content area */}
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
-    )
+    );
 }
 
 export default Sidebar;
