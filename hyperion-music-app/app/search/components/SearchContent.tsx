a92a2cee75033a0d5f0244e90157f3ece665edc1
DESKTOP-KH7DB9P\matha <techchintu2017@gmail.com>
2023-07-09 18:26:54 -0700

    Added a like feature
    This is a React Functional Component that allows users to like and unlike a song.
    The component uses the user's context and session context to track and modify the liked songs.

--- hyperion-music-app/app/search/components/SearchContent.tsx
+++ hyperion-music-app/app/search/components/SearchContent.tsx
@@ -1,50 +1,71 @@
+/**
+ * SearchContent.tsx:
+ *
+ * This is a React Functional Component that displays a list of songs returned by a search operation.
+ * If no songs are found, it shows a message stating that no songs were found.
+ * It uses two custom hooks: 'useOnPlay' to handle the song playing operation and a 'LikeButton' component to allow liking a song.
+ * 
+ * Details:
+ * 
+ * - The component receives a 'songs' prop which is an array of the songs to be displayed.
+ * - It uses the 'useOnPlay' hook with the list of songs, which returns a function to handle the song playing operation.
+ * - For each song in the 'songs' array, it renders a 'MediaItem' component and a 'LikeButton' component.
+ * - The 'MediaItem' component is given an 'onClick' handler which calls the 'onPlay' function with the song's id.
+ * - The 'LikeButton' component is given the song's id.
+ */
+
 "use client";
 
+// Import necessary packages and custom types
 import { Song } from "@/types";
 import MediaItem from "@/components/MediaItem";
 import LikeButton from "@/components/LikeButton";
 import useOnPlay from "@/hooks/useOnPlay";
 
-
+// Define the type of the props
 interface SearchContentProps {
     songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({
-    songs
-}) => {
+// This is a functional component which takes a list of songs and displays them
+const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+    // 'useOnPlay' is a custom hook that sets up the logic for playing the song
     const onPlay = useOnPlay(songs);
 
+    // If there are no songs, display a message stating no songs were located
     if (songs.length === 0) {
         return (
             <div
                 className="
-          flex 
-          flex-col 
-          gap-y-2 
-          w-full 
-          px-6 
-          text-neutral-400
-        "
+                    flex 
+                    flex-col 
+                    gap-y-2 
+                    w-full 
+                    px-6 
+                    text-neutral-400
+                "
             >
                 Sorry, no songs were located...
             </div>
         )
     }
 
+    // If there are songs, map through each song and render a MediaItem and a LikeButton for each song
     return (
         <div className="flex flex-col gap-y-2 w-full px-6">
             {songs.map((song: Song) => (
                 <div
-                    key={song.id}
+                    key={song.id}  // Unique key for each song item
                     className="flex items-center gap-x-4 w-full"
                 >
                     <div className="flex-1">
+                        {/* When a MediaItem is clicked, the 'onPlay' function is called with the song's id */}
                         <MediaItem
                             onClick={(id: string) => onPlay(id)}
                             data={song}
                         />
                     </div>
+                    {/* Render a LikeButton component for each song and pass the song's id to it */}
                     <LikeButton songId={song.id} />
                 </div>
             ))}
--- hyperion-music-app/components/LikeButton.tsx
+++ hyperion-music-app/components/LikeButton.tsx
@@ -1,64 +1,82 @@
+/**
+ * LikeButton.tsx:
+ *
+ * This is a React Functional Component that allows users to like and unlike a song.
+ * The component uses the user's context and session context to track and modify the liked songs.
+ * 
+ * Details:
+ * 
+ * - The component receives a 'songId' prop which is the id of the song.
+ * - It uses the 'useUser' hook to get the current user and the 'useSessionContext' to get the Supabase client.
+ * - It uses the 'useEffect' hook to fetch the user's liked songs when the component is rendered.
+ * - When the like button is clicked, the component checks if the user is authenticated. If not, it shows the authentication modal.
+ * - If the user is authenticated, it adds or removes the song from the user's liked songs based on the current like state.
+ * - After the like operation, it refreshes the page to show the updated like state.
+ */
+
 "use client";
 
+// Necessary imports
 import { useEffect, useState } from "react";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { toast } from "react-hot-toast";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
 
+// Define the props interface for LikeButton component
 interface LikeButtonProps {
     songId: string;
 };
 
-const LikeButton: React.FC<LikeButtonProps> = ({
-    songId
-}) => {
+const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
     const router = useRouter();
-    const {
-        supabaseClient
-    } = useSessionContext();
+    const { supabaseClient } = useSessionContext();
     const authModal = useAuthModal();
     const { user } = useUser();
 
+    // State to track if the song is liked by the user
     const [isLiked, setIsLiked] = useState<boolean>(false);
 
     useEffect(() => {
-        if (!user?.id) {
-            return;
+        // Fetch the liked songs of the user if the user is authenticated
+        if (user?.id) {
+            const fetchData = async () => {
+                const { data, error } = await supabaseClient
+                    .from('liked_songs')
+                    .select('*')
+                    .eq('user_id', user.id)
+                    .eq('song_id', songId)
+                    .single();
+
+                if (!error && data) {
+                    setIsLiked(true);
+                }
+            };
+
+            fetchData();
         }
-
-        const fetchData = async () => {
-            const { data, error } = await supabaseClient
-                .from('liked_songs')
-                .select('*')
-                .eq('user_id', user.id)
-                .eq('song_id', songId)
-                .single();
-
-            if (!error && data) {
-                setIsLiked(true);
-            }
-        }
-
-        fetchData();
     }, [songId, supabaseClient, user?.id]);
 
+    // Choose the appropriate icon based on the like state
     const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
 
     const handleLike = async () => {
+        // Check if the user is authenticated
         if (!user) {
+            // If not, show the authentication modal
             return authModal.onOpen();
         }
 
+        // Perform the appropriate like operation based on the current like state
         if (isLiked) {
             const { error } = await supabaseClient
                 .from('liked_songs')
                 .delete()
                 .eq('user_id', user.id)
-                .eq('song_id', songId)
+                .eq('song_id', songId);
 
             if (error) {
                 toast.error(error.message);
@@ -70,7 +88,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({
                 .from('liked_songs')
                 .insert({
                     song_id: songId,
-                    user_id: user.id
+                    user_id: user.id,
                 });
 
             if (error) {
@@ -81,21 +99,18 @@ const LikeButton: React.FC<LikeButtonProps> = ({
             }
         }
 
+        // Refresh the page to show the updated like state
         router.refresh();
-    }
+    };
 
     return (
         <button
-            className="
-        cursor-pointer 
-        hover:opacity-75 
-        transition
-      "
+            className="cursor-pointer hover:opacity-75 transition"
             onClick={handleLike}
         >
             <Icon color={isLiked ? '#4B0082' : 'white'} size={25} />
         </button>
     );
-}
+};
 
 export default LikeButton;
